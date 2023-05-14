1ee361d71b586787ae63e85af773f55aa3ef6b61
98jamieson <jamilemus@gmail.com>
2023-05-14 17:38:03 -0600

    changes1m1

--- Formulario1.Designer.cs
+++ Formulario1.Designer.cs
@@ -32,6 +32,8 @@ namespace ProyectGit
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.txtapellido = new System.Windows.Forms.Label();
+            this.txtAp = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -52,18 +54,36 @@ namespace ProyectGit
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(304, 184);
+            this.button1.Location = new System.Drawing.Point(320, 229);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "aceptar";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // txtapellido
+            // 
+            this.txtapellido.AutoSize = true;
+            this.txtapellido.Location = new System.Drawing.Point(251, 176);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(59, 13);
+            this.txtapellido.TabIndex = 3;
+            this.txtapellido.Text = "APELLIDO";
+            // 
+            // txtAp
+            // 
+            this.txtAp.Location = new System.Drawing.Point(320, 176);
+            this.txtAp.Name = "txtAp";
+            this.txtAp.Size = new System.Drawing.Size(100, 20);
+            this.txtAp.TabIndex = 4;
+            // 
             // Formulario1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAp);
+            this.Controls.Add(this.txtapellido);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -79,6 +99,8 @@ namespace ProyectGit
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txtapellido;
+        private System.Windows.Forms.TextBox txtAp;
     }
 }
 
