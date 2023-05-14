0269746ea8c9a506dd3f4c035ce3bd807d6e460b
98jamieson <jamilemus@gmail.com>
2023-05-14 17:26:28 -0600

    Primer commit

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ProyectGit
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Formulario1());
         }
     }
 }
